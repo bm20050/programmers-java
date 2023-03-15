268d1b0872b5c58689e01e75a79180968c433add
bm20050 <bm20050@gmail.com>
2023-03-15 22:49:50 +0900

    03.15 update

--- src/Lv2/예상대진표.java
+++ src/Lv2/예상대진표.java
@@ -4,13 +4,22 @@ public class 예상대진표 {
     public static int solution(int n, int a, int b) {
         int answer = 0;
 
-        return answer;
+        while (true) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+
+            if (a == b)
+                break;
+            answer ++;
+        }
+
+        return answer + 1;
     }
 
     public static void main(String[] args) {
         int n = 8;
         int a = 4;
-        int b = 7;
+        int b = 5;
 
         System.out.println(solution(n, a, b));
     }

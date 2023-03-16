c1ed17dfbfced93387d5d2114ada31eb74248bf9
bm20050 <bm20050@gmail.com>
2023-03-16 18:02:21 +0900

    03.16 update

--- src/Lv2/점프와순간이동.java
+++ src/Lv2/점프와순간이동.java
@@ -5,15 +5,16 @@ import java.util.*;
 public class 점프와순간이동 {
     public static int solution(int n) {
         int ans = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        String s = Integer.toBinaryString(n);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1')
+                ans += 1;
+        }
         return ans;
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5000;
 
         System.out.println(solution(n));
     }

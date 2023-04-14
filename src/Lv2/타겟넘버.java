605feceddc7b3cbdadf71b7cce9d6df8eac3a3d4
bm20050 <bm20050@gmail.com>
2023-04-14 10:01:53 +0900

    Apply .gitignore

---
+++ src/Lv2/타겟넘버.java
@@ -0,0 +1,15 @@
+package Lv2;
+
+public class 타겟넘버 {
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 3;
+
+        System.out.println(solution(numbers, target));
+    }
+}
---
+++ src/Lv2/프린터.java
@@ -0,0 +1,16 @@
+package Lv2;
+
+public class 프린터 {
+    public static int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] priorities = {2, 1, 3, 2};
+        int location = 2;
+
+        System.out.println(solution(priorities, location));
+    }
+}
